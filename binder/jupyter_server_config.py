a8140b3fe5f965362bd2585266614fd1bbc69b14
Elsa Culler <elsa.culler@oulu.fi>
2026-07-23 13:40:48 +0000

    Add text editors and configure VSCode workspace

--- binder/jupyter_server_config.py
+++ binder/jupyter_server_config.py
@@ -1,3 +1,7 @@
 import os
+from pathlib import Path
+
+workspace = Path.home() / "diwa-community"
 
 os.environ.setdefault("CODE_DISABLE_PASSWORD", "true")
+os.environ["CODE_WORKING_DIRECTORY"] = str(workspace)
